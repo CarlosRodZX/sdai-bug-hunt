bb2135275f9df89cc3bf250e3aaac259b864752e
CarlosRodZX <150416580+CarlosRodZX@users.noreply.github.com>
2024-08-29 02:58:48 +0000

    Bug hunt

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-getElementById('funfactButton').addEventListener('click', function() {
-  const funFact = document.getElementById('funfact');
-  if (funFact.style.display === 'none') {
-      funFact.style.display = 'block';
-  } else {
-      funFact.style.display = 'none';
+  document.getElementById('funFactButton').addEventListener('click', function () {
+   const funFact = document.getElementById('funfact');
+
+    if (funFact.style.display === 'none') {
+
+       funFact.style.display = 'block';
+
+   } else {
+
+       funFact.style.display = 'none';
   }
-});
+
+ });
+
+
